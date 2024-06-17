e2dc7be2b5149fe94d302f053d2245c72bc1787b
Udhaykrishn <krishnauday320@gmail.com>
2024-06-17 21:46:27 +0530

    change the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PageForm (@CodeWithKliton)",
+  title: "FormNest",
   description: "Check my youtube channel for more content",
 };
 
